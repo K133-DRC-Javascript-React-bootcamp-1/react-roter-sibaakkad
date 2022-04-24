5d76b097686643ad0b85b8ad6ba6337394b0aa24
Siba Akkad <sibaakkad98@gmail.com>
2022-04-24 04:41:23 +0300

    route integration

---
+++ src/components/home.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+function Home(){
+  return (
+    <div>
+  <div class="container">
+    <h2 >Ana Sayfa</h2>
+    <h3>Sitemizdeki Bulunan Müzik Aletleri </h3>
+  </div> 
+ <div class="muzik-container">
+ 
+  <ul class="muzik-aletleri" >
+    <li>Gitar</li>
+    <li>Davul</li>
+    <li>Piyano</li>
+    <li>Kaman</li>
+  </ul> 
+ </div>
+    </div>
+  );
+}
+export default Home;
---
+++ src/components/notAuthorized.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function NotAuthorized() {
+
+  return (
+    <>
+      <div>
+        <h1>You Are Not Authorized</h1>
+      </div>
+    </>);
+}
+export default NotAuthorized;
